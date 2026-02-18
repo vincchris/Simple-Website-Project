7ee347a4c43182d37080e89a7badaee94d1bc39f
Vincent Chriss <vchristianthen@gmail.com>
2026-02-18 19:28:33 +0700

    Update Type 3 Project

--- LiveCharacterCounter/script.js
+++ LiveCharacterCounter/script.js
@@ -1,1 +1,11 @@
 let inputText = document.getElementById("inputTxt")
+let result = document.getElementById("result")
+
+inputText.addEventListener('input', () => {
+  let count = inputText.value.length
+  result.textContent = count
+
+  if (count === 0) {
+    result.textContent = 0
+  }
+})
