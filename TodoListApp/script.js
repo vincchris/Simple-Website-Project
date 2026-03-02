d425f01cc99837497645c86b3c57789fa875bb1e
Vincent Chriss <vchristianthen@gmail.com>
2026-03-02 19:10:48 +0700

    Type 4 Progress Done

--- TodoListApp/script.js
+++ TodoListApp/script.js
@@ -1,3 +1,10 @@
-let inputTdl = document.getElementById('inputTdl')
-let sendInput = document.getElementById("SendInput")
-let checkboxResult = document.getElementById("checkboxResult")
+let input = document.getElementById("inputTdl")
+let button = document.getElementById("sendInput")
+let list = document.getElementById("listContainer")
+
+button.addEventListener("click", () => {
+  let li = document.createElement("li")
+  li.textContent = input.value
+  list.appendChild(li)
+  input.value = ""
+})
