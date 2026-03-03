70e859b51586c7e94718bb71a9f7ff05c4c1b08f
Vincent Chriss <vchristianthen@gmail.com>
2026-03-03 20:39:48 +0700

    Update Progress Type 4

--- TodoListApp/script.js
+++ TodoListApp/script.js
@@ -3,8 +3,36 @@ let button = document.getElementById("sendInput")
 let list = document.getElementById("listContainer")
 
 button.addEventListener("click", () => {
+
+  if (input.value.trim() === "") return
+
+  // Buat elemen li
   let li = document.createElement("li")
-  li.textContent = input.value
+
+  // Buat checkbox
+  let checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+
+  // Buat span untuk text
+  let span = document.createElement("span")
+  span.textContent = input.value
+
+  // Jika checkbox dicentang → kasih garis
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      span.style.textDecoration = "line-through"
+    } else {
+      span.style.textDecoration = "none"
+    }
+  })
+
+  // Masukkan checkbox + text ke li
+  li.appendChild(checkbox)
+  li.appendChild(span)
+
+  // Masukkan li ke list
   list.appendChild(li)
+
+  // Kosongkan input
   input.value = ""
 })
